873fc565aac0eb95f8d7ac716871c0b5336df077
ViniciusDugue <103336607+ViniciusDugue@users.noreply.github.com>
2023-12-30 23:31:40 -0800

    Hash Space Project 7
    Made 3D objects with parameterized surfaces, hashing function to create pseudorandom color and displacement of surface points using surface normals, shape interface to handle multiple shapes/surfaces

--- Assets/Scripts/HashVisualization.cs
+++ Assets/Scripts/HashVisualization.cs
@@ -6,86 +6,166 @@ using UnityEngine;
 
 using static Unity.Mathematics.math;
 
-public class HashVisualization : MonoBehaviour 
-{
+public class HashVisualization : MonoBehaviour {
 
 	[BurstCompile(FloatPrecision.Standard, FloatMode.Fast, CompileSynchronously = true)]
-	struct HashJob : IJobFor 
-    {
+	struct HashJob : IJobFor {
 
-		[WriteOnly] public NativeArray<uint> hashes;
-        
-		public int resolution;
+		[ReadOnly]
+		public NativeArray<float3x4> positions;
 
-		public float invResolution;
-        public SmallXXHash hash;
+		[WriteOnly]
+		public NativeArray<uint4> hashes;
 
-        // takes in index of a point, calculates uv coordinates and uses uvs to calculate hash
-        // value and stores hash value in native array
+		public SmallXXHash4 hash;
+
+		public float3x4 domainTRS;
+
+		// takes a transformation matrix and multiplies it to a position matrix that contains 
+		//4 xyz position vectors
+		float4x3 TransformPositions (float3x4 trs, float4x3 p) => float4x3(
+			trs.c0.x * p.c0 + trs.c1.x * p.c1 + trs.c2.x * p.c2 + trs.c3.x,
+			trs.c0.y * p.c0 + trs.c1.y * p.c1 + trs.c2.y * p.c2 + trs.c3.y,
+			trs.c0.z * p.c0 + trs.c1.z * p.c1 + trs.c2.z * p.c2 + trs.c3.z
+		);
+
+		// transforms position matrix and calculates integer vectors to generate hash value
+		// position vector is transposed because vectorized operations on vectors are done by doing 
+		// all x y then z calculations
 		public void Execute(int i) {
-			int v = (int)floor(invResolution * i + 0.00001f);
-			int u = i - resolution * v - resolution / 2;
-			v -= resolution / 2;
+			float4x3 p = TransformPositions(domainTRS, transpose(positions[i]));
 
-			hashes[i] = hash.Eat(u).Eat(v);
+			int4 u = (int4)floor(p.c0);
+			int4 v = (int4)floor(p.c1);
+			int4 w = (int4)floor(p.c2);
+
+			hashes[i] = hash.Eat(u).Eat(v).Eat(w);
 		}
 	}
 
-    static int
-		hashesId = Shader.PropertyToID("_Hashes"),
+	public enum Shape { Plane, Sphere, Torus }
+
+	static Shapes.ScheduleDelegate[] shapeJobs = {
+		Shapes.Job<Shapes.Plane>.ScheduleParallel,
+		Shapes.Job<Shapes.Sphere>.ScheduleParallel,
+		Shapes.Job<Shapes.Torus>.ScheduleParallel
+	};
+
+	static int
+		hashesId = Shader.PropertyToID("_Hashes"),// stores hash values of each object
+		positionsId = Shader.PropertyToID("_Positions"),// stores new positions of each object
+		normalsId = Shader.PropertyToID("_Normals"),// stores surface normals of each object for displacement
 		configId = Shader.PropertyToID("_Config");
 
 	[SerializeField] Mesh instanceMesh;
+
 	[SerializeField] Material material;
+
+	[SerializeField] Shape shape;
+
+	[SerializeField, Range(0.1f, 10f)] float instanceScale = 2f;
+
 	[SerializeField, Range(1, 512)] int resolution = 16;
-    [SerializeField] int seed;
-    [SerializeField, Range(-2f, 2f)] float verticalOffset = 1f;
-	NativeArray<uint> hashes;
 
-	ComputeBuffer hashesBuffer;
+	[SerializeField, Range(-0.5f, 0.5f)] float displacement = 0.1f;
 
-	MaterialPropertyBlock propertyBlock;    
+	[SerializeField] int seed;
 
-    void OnEnable () 
-    {
+	// domain is the actual surface of the shape we are creating
+	// transformations done on the points of the surface is done by multiplying the domain matrix 
+	[SerializeField] SpaceTRS domain = new SpaceTRS {scale = 8f};
+
+	NativeArray<uint4> hashes;
+
+	NativeArray<float3x4> positions, normals;
+
+	ComputeBuffer hashesBuffer, positionsBuffer, normalsBuffer;
+
+	// a material properties memory block for setting per instance material properties for all objects
+	// the material property block passes compute buffers to shaders
+	MaterialPropertyBlock propertyBlock;
+
+	bool isDirty;
+
+	Bounds bounds;
+
+	void OnEnable () {
+		isDirty = true;
+
 		int length = resolution * resolution;
-		hashes = new NativeArray<uint>(length, Allocator.Persistent);
-		hashesBuffer = new ComputeBuffer(length, 4);
-
-		new HashJob {
-			hashes = hashes,
-			resolution = resolution,
-			invResolution = 1f / resolution,
-            hash = SmallXXHash.Seed(seed)
-		}.ScheduleParallel(hashes.Length, resolution, default).Complete();
-
-		hashesBuffer.SetData(hashes);
+		length = length / 4 + (length & 1);
+		hashes = new NativeArray<uint4>(length, Allocator.Persistent);
+		positions = new NativeArray<float3x4>(length, Allocator.Persistent);
+		normals = new NativeArray<float3x4>(length, Allocator.Persistent);
+		hashesBuffer = new ComputeBuffer(length * 4, 4);
+		positionsBuffer = new ComputeBuffer(length * 4, 3 * 4);
+		normalsBuffer = new ComputeBuffer(length * 4, 3 * 4);
 
 		propertyBlock ??= new MaterialPropertyBlock();
 		propertyBlock.SetBuffer(hashesId, hashesBuffer);
-		propertyBlock.SetVector(configId, new Vector4(resolution, 1f / resolution, verticalOffset / resolution));
+		propertyBlock.SetBuffer(positionsId, positionsBuffer);
+		propertyBlock.SetBuffer(normalsId, normalsBuffer);
+		propertyBlock.SetVector(configId, new Vector4(
+			resolution, instanceScale / resolution, displacement
+		));
 	}
 
-    void OnDisable () 
-    {
+	void OnDisable () {
 		hashes.Dispose();
+		positions.Dispose();
+		normals.Dispose();
 		hashesBuffer.Release();
+		positionsBuffer.Release();
+		normalsBuffer.Release();
 		hashesBuffer = null;
+		positionsBuffer = null;
+		normalsBuffer = null;
 	}
 
 	void OnValidate () {
-		if (hashesBuffer != null && enabled) 
-        {
+		if (hashesBuffer != null && enabled) {
 			OnDisable();
 			OnEnable();
 		}
 	}
 
-    void Update () 
-    {
+	void Update () {
+		// isDirty checks if the state of object has changed and if needs to updated
+		// avoids unnecesary recalculateions and updates when no changes have occured
+		if (isDirty || transform.hasChanged) {
+			isDirty = false;
+			transform.hasChanged = false;
+
+			JobHandle handle = shapeJobs[(int)shape](
+				positions, normals, resolution, transform.localToWorldMatrix, default
+			);
+
+			new HashJob {
+				positions = positions,
+				hashes = hashes,
+				hash = SmallXXHash4.Seed(seed),
+				domainTRS = domain.Matrix
+			}.ScheduleParallel(hashes.Length, resolution, handle).Complete();
+
+			// reinterprets value in hashes, positions to a new value to unvectorize it
+			// takes positions float3x4 matrix and reinterprets it into 4 position vectors
+			// of 4 byte size
+			hashesBuffer.SetData(hashes.Reinterpret<uint>(4 * 4));
+			positionsBuffer.SetData(positions.Reinterpret<float3>(3 * 4 * 4));
+			normalsBuffer.SetData(normals.Reinterpret<float3>(3 * 4 * 4));
+
+			// sets a bounding box for all game objects. bound is updated with displacement
+			bounds = new Bounds(
+				transform.position,
+				float3(2f * cmax(abs(transform.lossyScale)) + displacement)
+			);
+		}
+
 		Graphics.DrawMeshInstancedProcedural(
-			instanceMesh, 0, material, new Bounds(Vector3.zero, Vector3.one),
-			hashes.Length, propertyBlock
+			instanceMesh, 0, material, bounds, resolution * resolution, propertyBlock
 		);
 	}
 }
+
+// takes in index of a point, calculates uv coordinates and uses uvs to calculate hash
+// value and stores hash value in native array
--- Assets/Scripts/SmallXXHash.cs
+++ Assets/Scripts/SmallXXHash.cs
@@ -1,3 +1,4 @@
+using Unity.Mathematics;
 
 // its readonly so the methods dont adjust accumulator value they are called on
 // this makes accumulator value immutable
@@ -17,6 +18,12 @@ public readonly struct SmallXXHash {
 
     // Seed method that returns a seed plus a prime
     public static SmallXXHash Seed (int seed) => (uint)seed + primeE;
+
+    // shifting bits causes bits on the left to be lost and are padedd with zeros on the right
+    // rotating bits shifts them but the bits on the left are not lost and instead go to right
+    // rotates bits of accumulator to the left by shifting all bits to the left with << and
+    // adds the shifted bits to the right
+    static uint RotateLeft (uint data, int steps) => (data << steps) | (data >> 32 - steps);
 
     // consuming data in groups of 32 bits 
     // =>: says this method has a single expression and the result of the expression is 
@@ -45,13 +52,43 @@ public readonly struct SmallXXHash {
     public static implicit operator SmallXXHash (uint accumulator) =>
 		new SmallXXHash(accumulator);
     
-    // shifting bits causes bits on the left to be lost and are padedd with zeros on the right
-    // rotating bits shifts them but the bits on the left are not lost and instead go to right
-    // rotates bits of accumulator to the left by shifting all bits to the left with << and
-    // adds the shifted bits to the right
-    static uint RotateLeft (uint data, int steps) => (data << steps) | (data >> 32 - steps);
 }
 
+//to support automatic vectorization,we need to vectorize smallxxhash
+//do this by changing operations on int float and uint3 float3 types to uint4,float4, and int4
+public readonly struct SmallXXHash4 
+{
+	const uint primeB = 0b10000101111010111100101001110111;
+	const uint primeC = 0b11000010101100101010111000111101;
+	const uint primeD = 0b00100111110101001110101100101111;
+	const uint primeE = 0b00010110010101100110011110110001;
+
+	readonly uint4 accumulator;
+
+	public SmallXXHash4 (uint4 accumulator) {
+		this.accumulator = accumulator;
+	}
+
+	public static implicit operator SmallXXHash4 (uint4 accumulator) =>
+		new SmallXXHash4(accumulator);
+
+	public static SmallXXHash4 Seed (int4 seed) => (uint4)seed + primeE;
+
+	static uint4 RotateLeft (uint4 data, int steps) =>
+		(data << steps) | (data >> 32 - steps);
+
+	public SmallXXHash4 Eat (int4 data) => RotateLeft(accumulator + (uint4)data * primeC, 17) * primeD;
+
+	public static implicit operator uint4 (SmallXXHash4 hash) {
+		uint4 avalanche = hash.accumulator;
+		avalanche ^= avalanche >> 15;
+		avalanche *= primeB;
+		avalanche ^= avalanche >> 13;
+		avalanche *= primeC;
+		avalanche ^= avalanche >> 16;
+		return avalanche;
+	}
+}
 
 
 
